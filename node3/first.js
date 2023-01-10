c41cbf0fd915bd058aa33a6e1706c0b3a7137e9e
Yakumov <dgahar.yakumov@gmail.com>
2023-01-10 16:22:16 +0300

    я сделал задание nod js

---
+++ node3/first.js
@@ -0,0 +1,13 @@
+
+
+
+function sum (a, b){
+    return a + b
+}
+console.log(sum(5, 5))
+
+
+
+export default function(a, b){
+    return a + b 
+}
---
+++ node3/second.js
@@ -0,0 +1,5 @@
+
+
+import sum from './first.js';
+
+console.log(sum);
